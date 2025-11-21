8d2dac0c6c37c65e6369f47f9232ca3e7f64f3b6
Carlos CC <carloscc9297@gmail.com>
2025-11-21 14:06:51 +0100

    :construction::lipstick: working on ux ui lobby

--- src/renderer/src/pages/Lobby.tsx
+++ src/renderer/src/pages/Lobby.tsx
@@ -8,16 +8,19 @@ const Lobby: FC = () => {
     }
     return (
         <>
-            <div id='envoltorio-contenido-lobby' className='border p-4 flex flex-col justify-start'>
+            <div id='envoltorio-contenido-lobby' className=' w-screen h-screen border border-purple-700 px-4 flex flex-col justify-start'>
                 <button onClick={goToHome} className=' w-20 rounded-md border-2 border-green-500 ml-4 px-4 py-2 text-green-500 hover:bg-green-500 hover:text-white transition'>
                     Home
                 </button>
-                <div id='envoltorio-contenido-lobby' className='border border-yellow-400 w-screen h-screen flex justify-center '>
-                    <div id='titulo-lobby' className='border border-red-500 w-screen'>
-                        <div className='border border-blue-600  bg-sky-500 flex justify-center m-28 rounded-md '>
+                <div id='envoltorio-contenido-lobby' className='border border-yellow-400  flex justify-center w-full '>
+                    <div id='titulo-lobby' className='w-full border border-red-500 '>
+                        <div className='border border-blue-600  bg-sky-500 flex justify-center mt-4 rounded-md '>
                             <p>Página del lobby</p>
                         </div>
-                    <p>hola </p>
+                        <div className='border flex flex-row' >
+                            <p className='border border-lime-500 w-[40vw]' >izquierd </p>
+                            <p className='w-[60vw] border'>derecho</p>
+                        </div>
                     </div>
                 </div>
             </div>
