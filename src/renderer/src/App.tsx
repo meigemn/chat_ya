5276ad03d37950b4a55004017b5e8fe6a1b33dcd
Carlos CC <carloscc9297@gmail.com>
2025-11-21 12:25:45 +0100

    :sparkles: new Lobby page. Navegation between Home and Lobby

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,48 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Lobby from './pages/Lobby';
 
 function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
-    <div className="bg-blue-500 text-blue p-4">
-      <h1 className="text-2xl font-bold">Chat YA</h1>
-      <p className="mt-4">Tailwind CSS está funcionando!</p>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/lobby" element={<Lobby />} />
+    </Routes>
+  );
 }
 
-export default App
+function Home() {
+  const navigate = useNavigate();
+
+  const goToLobby = () => {
+    navigate('/lobby');
+  };
+
+  return (
+    <div id='envoltorio-icono-texto-boton' className='border border-red-600'>
+      <div id='envoltorio-icono-texto' className='border flex flex-col items-center w-60'>
+        <div id='icono-chat-ya' className='bg-blue-400 rounded-full w-20 h-20 flex items-center justify-center text-white'>
+          icono
+        </div>
+        <div id='titulo-y-descripción' className='flex flex-col items-center justify-center'>
+          <div id='titulo-chat-ya' className='mt-6'>
+            Chat ya
+          </div>
+          <div id='descripcion-chat-ya'>
+            Chat en tiempo real
+          </div>
+        </div>
+      </div>
+      <div id='boton-redireccion-lobby' className='flex justify-start mt-4'>
+        <button
+          onClick={goToLobby}
+          className='rounded-full border-2 border-blue-500 px-4 py-2 text-blue-500 hover:bg-blue-500 hover:text-white transition'
+        >
+          Lobby
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
--- src/renderer/src/pages/Lobby.tsx
+++ src/renderer/src/pages/Lobby.tsx
@@ -1,12 +1,22 @@
-import {FC} from 'react';
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Lobby: FC =() =>{
-    return(
-    <div>
-        <p>
-            pagina del lobby
-        </p>
-    </div>
+const Lobby: FC = () => {
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate('/');
+    }
+    return (
+        <>
+            <button onClick={goToHome}  className='rounded-full border-2 border-green-500 px-4 py-2 text-green-500 hover:bg-green-500 hover:text-white transition'>
+                Home
+            </button>
+            <div id='titulo-lobby-y-boton' className='border border-red-500'>
+            <div className='border border-blue-600'>
+                <p>Página del lobby</p>
+            </div>
+            </div>
+        </>
     )
 }
 export default Lobby;
