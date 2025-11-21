f7db11e32fb7944f796bd4280f7e6f02ccc668e6
Carlos CC <carloscc9297@gmail.com>
2025-11-21 13:35:05 +0100

    :construction::lipstick: working on ux ui lobby

--- src/renderer/src/pages/Lobby.tsx
+++ src/renderer/src/pages/Lobby.tsx
@@ -8,13 +8,18 @@ const Lobby: FC = () => {
     }
     return (
         <>
-            <button onClick={goToHome}  className='rounded-full border-2 border-green-500 px-4 py-2 text-green-500 hover:bg-green-500 hover:text-white transition'>
-                Home
-            </button>
-            <div id='titulo-lobby-y-boton' className='border border-red-500'>
-            <div className='border border-blue-600'>
-                <p>Página del lobby</p>
-            </div>
+            <div id='envoltorio-contenido-lobby' className='border p-4 flex flex-col justify-start'>
+                <button onClick={goToHome} className=' w-20 rounded-md border-2 border-green-500 ml-4 px-4 py-2 text-green-500 hover:bg-green-500 hover:text-white transition'>
+                    Home
+                </button>
+                <div id='envoltorio-contenido-lobby' className='border border-yellow-400 w-screen h-screen flex justify-center '>
+                    <div id='titulo-lobby' className='border border-red-500 w-screen'>
+                        <div className='border border-blue-600  bg-sky-500 flex justify-center m-28 rounded-md '>
+                            <p>Página del lobby</p>
+                        </div>
+                    <p>hola </p>
+                    </div>
+                </div>
             </div>
         </>
     )
